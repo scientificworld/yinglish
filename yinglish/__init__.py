b9f4f8df36bf428ce18de442f64d753896d8030e
scientificworld <scientificworld@126.com>
2022-08-06 00:23:25 +0800

    hso

--- yinglish/__init__.py
+++ yinglish/__init__.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 import jieba
 import jieba.posseg as pseg
@@ -6,12 +7,13 @@ jieba.setLogLevel(20)
 
 
 def _词转换(x, y, 淫乱度):
+#   print(x,y)
     if random.random() > 淫乱度:
         return x
-    if x in {'，', '。'}:
-        return '……'
     if x in {'!', '！'}:
         return '❤'
+    if y == 'x':
+        return ['……嗯……', '……不要……', '……哈啊……', '……呜喵……', '……要…去了……'][math.ceil(淫乱度 * 5) - 1]
     if len(x) > 1 and random.random() < 0.5:
         return f'{x[0]}……{x}'
     else:
@@ -21,6 +23,7 @@ def _词转换(x, y, 淫乱度):
 
 
 def chs2yin(s, 淫乱度=0.5):
+    if 淫乱度 > 1 or 淫乱度 < 0: 淫乱度 = 0.5
     return ''.join([_词转换(x, y, 淫乱度) for x, y in pseg.cut(s)])
 
 
